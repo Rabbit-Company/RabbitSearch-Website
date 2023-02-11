5f66c0bf2d762451d9be39beba49b829ecda2b89
Ziga <ziga.zajc007@gmail.com>
2023-02-11 15:49:27 +0100

    Update

--- website/js/index.js
+++ website/js/index.js
@@ -1,3 +1,10 @@
 if(parms.get('q') !== null){
 	document.getElementById('search').value = parms.get('q');
+	search(parms.get('q'));
+}
+
+function search(query, type){
+	fetch('https://api.rabbitsearch.org?q=' + encodeURIComponent(query)).then(res => {
+		console.log(res.body);
+	});
 }

28433bfcf42f01d318cb85ce2bd0ae25a57f0a45
Ziga <ziga.zajc007@gmail.com>
2023-02-11 17:30:29 +0100

    Update

--- website/js/index.js
+++ website/js/index.js
@@ -1,17 +1,28 @@
 if(parms.get('q') !== null){
 	document.getElementById('search').value = parms.get('q');
 	search(parms.get('q')).then((data) => {
-		console.log(data);
+		displayResults(data);
 	}).catch((error) => {
 		console.log("Error: " + error);
 	});
 }
 
-function search(query, type){
+function search(query, type = 'search'){
 	return new Promise((resolve, reject) => {
 		fetch('https://api.rabbitsearch.org/search?q=' + encodeURIComponent(query))
 		.then((response) => response.json())
 		.then((data) => resolve(data))
 		.catch((error) => reject(error));
 	});
+}
+
+function displayResults(data, type = 'search'){
+	if(data.error !== 0) return;
+	if(typeof(data.webPages?.value) !== 'object') return;
+
+	let html = "";
+	for(let i = 0; i < data.webPages.value.length; i++){
+		html += `<p>${data.webPages.value[i].name}</p>`;
+	}
+	document.getElementById('results').innerHTML = html;
 }

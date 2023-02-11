90d5921340a6372947e6925be0003c8cee2b3b40
Ziga <ziga.zajc007@gmail.com>
2023-02-11 16:30:37 +0100

    Update

--- website/js/index.js
+++ website/js/index.js
@@ -1,10 +1,17 @@
 if(parms.get('q') !== null){
 	document.getElementById('search').value = parms.get('q');
-	search(parms.get('q'));
+	search(parms.get('q')).then((data) => {
+		console.log(data);
+	}).catch((error) => {
+		console.log("Error: " + error);
+	});
 }
 
 function search(query, type){
-	fetch('https://api.rabbitsearch.org/search?q=' + encodeURIComponent(query)).then(res => {
-		console.log(res.body);
+	return new Promise((resolve, reject) => {
+		fetch('https://api.rabbitsearch.org/search?q=' + encodeURIComponent(query))
+		.then((response) => response.json())
+		.then((data) => resolve(data))
+		.catch((error) => reject(error));
 	});
 }

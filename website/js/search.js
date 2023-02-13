c7809bbb782ae69d856c4bf25d52c87f6dbcc296
Ziga <ziga.zajc007@gmail.com>
2023-02-13 13:30:12 +0100

    Update

--- website/js/search.js
+++ website/js/search.js
@@ -156,7 +156,7 @@ function displayGeneralResults(results){
 			<path d="M5 7.2a2.2 2.2 0 0 1 2.2 -2.2h1a2.2 2.2 0 0 0 1.55 -.64l.7 -.7a2.2 2.2 0 0 1 3.12 0l.7 .7c.412 .41 .97 .64 1.55 .64h1a2.2 2.2 0 0 1 2.2 2.2v1c0 .58 .23 1.138 .64 1.55l.7 .7a2.2 2.2 0 0 1 0 3.12l-.7 .7a2.2 2.2 0 0 0 -.64 1.55v1a2.2 2.2 0 0 1 -2.2 2.2h-1a2.2 2.2 0 0 0 -1.55 .64l-.7 .7a2.2 2.2 0 0 1 -3.12 0l-.7 -.7a2.2 2.2 0 0 0 -1.55 -.64h-1a2.2 2.2 0 0 1 -2.2 -2.2v-1a2.2 2.2 0 0 0 -.64 -1.55l-.7 -.7a2.2 2.2 0 0 1 0 -3.12l.7 -.7a2.2 2.2 0 0 0 .64 -1.55v-1" fill="currentColor"></path>
  		</svg>
 
-		${escapeHtml(results.data.webPages.value[i].name)}</a>
+		<span>${escapeHtml(results.data.webPages.value[i].name)}</span></a>
 		<p class="text-green-600 text-base truncate">${escapeHtml(results.data.webPages.value[i].url)}</p>
 		<p class="secondaryColor text-sm">${escapeHtml(results.data.webPages.value[i].snippet)}</p>`;
 

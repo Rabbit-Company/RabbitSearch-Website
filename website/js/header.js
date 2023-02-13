fc07a6ceb138e4801322639a761e84774a9ad1a8
Ziga <ziga.zajc007@gmail.com>
2023-02-13 12:37:13 +0100

    Update

--- website/js/header.js
+++ website/js/header.js
@@ -11,14 +11,4 @@ function setup(){
 	document.getElementById("css-theme").href = "css/themes/" + localStorage.getItem('theme') + ".css";
 }
 
-document.onkeydown = function(e) {
-	if(e.key == "F12") return false;
-	if(e.ctrlKey && e.shiftKey && e.key == 'I') return false;
-	if(e.ctrlKey && e.shiftKey && e.key == 'C') return false;
-	if(e.ctrlKey && e.shiftKey && e.key == 'J') return false;
-	if(e.ctrlKey && (e.key == 'u' || e.key == 'U')) return false;
-}
-
-document.addEventListener('contextmenu', e => e.preventDefault());
-
 setup();

196d0c856287de5502522339874e86e33521a85f
Ziga <ziga.zajc007@gmail.com>
2023-02-14 19:15:46 +0100

    Update

--- website/js/search.js
+++ website/js/search.js
@@ -219,7 +219,7 @@ function displayImageResults(results){
 		html += `
 			<div class="group aspect-w-10 aspect-h-7 block w-full overflow-hidden rounded-lg bg-gray-100 focus:outline-none">
 				<a href="${escapeHtml(results.data.value[i].contentUrl)}">
-					<img id="IMG-${results.data.value[i].imageId}" src="${escapeHtml(results.data.value[i].contentUrl)}" alt="${name}" loading="lazy" class="loadedImages pointer-events-none object-cover group-hover:opacity-75">
+					<img id="${results.data.value[i].imageId}" src="${escapeHtml(results.data.value[i].contentUrl)}" alt="${name}" loading="lazy" class="loadedImages pointer-events-none object-cover group-hover:opacity-75">
 				</a>
 			</div>
 			<a href="${escapeHtml(results.data.value[i].hostPageUrl)}" class="tertiaryColor mt-2 block truncate text-sm font-medium">${name}</a>
